7cdfde218c6b3aba3b3acf6b0e8733b752a5ef0d
興怡 <549739+patterns@users.noreply.github.com>
2025-01-13 22:20:12 -0800

    template (#2)
    Co-authored-by: 興怡 <>

---
+++ ace/eep567/idx-template.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }: {
+  packages = [
+    pkgs.uv
+  ];
+  bootstrap = ''    
+    mkdir "$out"
+    mkdir -p "$out/.idx/"
+    cp -rf ${./dev.nix} "$out/.idx/dev.nix"
+    chmod -R +w "$out"
+  '';
+}
