e1e57c117bc9853befdf07f60fe1104d88565ac6
興怡 <>
2025-01-14 06:39:20 -0800

    cargo for deps

--- dev.nix
+++ dev.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
-  ####channel = "stable-24.05"; # or "unstable"
-  channel = "unstable-24.11";
+  channel = "stable-24.05"; # or "unstable"
+  ####channel = "unstable-24.11";
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    pkgs.uv
+    pkgs.cargo
   ];
   # Sets environment variables in the workspace
   env = {};
@@ -16,8 +16,17 @@
     workspace = {
       # Runs when a workspace is first created with this `dev.nix` file
       onCreate = {
-        install =
-          "uv init hello-world && cd hello-world && uv add --dev ipykernel pandas matplotlib && uv run ipython kernel install --user --name=hello-world";
+        installDependencies = "cargo install --git https://github.com/astral-sh/uv uv"
+        ####install =
+        ####  "uv init hello-world";
+        setup-project = ''
+          uv init hello-world
+          cd hello-world
+          uv add --dev ipykernel pandas matplotlib
+          uv run ipython kernel install --user --name=hello-world
+        '';
+
+
         # Open editors for the following files by default, if they exist:
         default.openFiles = [ "hello.py" ];
       };
