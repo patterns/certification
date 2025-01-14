624b765da0e2c8da8890d1f62bf2ce2e4f7ed330
興怡 <>
2025-01-14 05:15:44 -0800

    template

---
+++ ace/eep567/dev.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.uv
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+
+
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [ "ms-python.python" "ms-toolsai.jupyter" ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        install =
+          "uv init hello-world && cd hello-world && uv add --dev ipykernel pandas matplotlib && uv run ipython kernel install --user --name=hello-world";
+        # Open editors for the following files by default, if they exist:
+        default.openFiles = [ "hello.py" ];
+      };
+    };
+  };
+}
