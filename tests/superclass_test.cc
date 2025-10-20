734554f1c0a2fab98aef68a70e48a13cba119ebd
興怡 <549739+patterns@users.noreply.github.com>
2025-10-19 23:45:46 -0700

    Create superclass_test.cc

---
+++ tests/superclass_test.cc
@@ -0,0 +1,115 @@
+#include <gtest/gtest.h>
+
+#include "SortedList.h"
+
+// Superclass behavior
+TEST(SuperclassTest, empty_list) {
+   SortedList<> ls = SortedList();
+
+   // empty list
+   EXPECT_EQ(ls.isEmpty(), true);
+   EXPECT_EQ(ls.getCurrentSize(), 0);
+
+   // count does not change
+   EXPECT_EQ(ls.getTraverseCount(), 0);
+   ls.add(55);
+   EXPECT_EQ(ls.getTraverseCount(), 0);
+}
+
+TEST(SuperclassTest, node_creation) {
+   SortedList<> ls = SortedList();
+   bool added = ls.add(55);
+
+   EXPECT_EQ(added, true);
+   EXPECT_EQ(ls.isEmpty(), false);
+   EXPECT_EQ(ls.getCurrentSize(), 1);
+   EXPECT_EQ(ls.retrieve(0), 55);
+
+   added = ls.add(77);
+
+   EXPECT_EQ(added, true);
+   EXPECT_EQ(ls.getCurrentSize(), 2);
+   EXPECT_EQ(ls.retrieve(0), 77);
+
+   added = ls.add(22);
+   EXPECT_EQ(added, true);
+   EXPECT_EQ(ls.getCurrentSize(), 3);
+   EXPECT_EQ(ls.retrieve(0), 22);
+
+   // duplicates are ignored
+   added = ls.add(77);
+   EXPECT_EQ(added, true);
+   EXPECT_EQ(ls.getCurrentSize(), 3);
+   EXPECT_EQ(ls.retrieve(0), 22);
+
+   // no negatives
+   added = ls.add(-10);
+   EXPECT_EQ(added, false);
+   EXPECT_EQ(ls.retrieve(100), ERROR_INDEX);
+}
+
+TEST(SuperclassTest, list_reset) {
+   SortedList<> ls = SortedList();
+   bool added = ls.add(55);
+
+   EXPECT_EQ(added, true);
+
+   ls.clear();
+
+   EXPECT_EQ(ls.isEmpty(), true);
+}
+
+TEST(SuperclassTest, node_deletion) {
+   SortedList<> ls = SortedList();
+
+   bool added = ls.add(55);
+
+   EXPECT_EQ(added, true);
+   EXPECT_EQ(ls.getCurrentSize(), 1);
+   EXPECT_EQ(ls.isEmpty(), false);
+
+   bool remed = ls.remove(55);
+   EXPECT_EQ(remed, true);
+   EXPECT_EQ(ls.isEmpty(), true);
+
+   added = ls.add(55);
+   added = ls.add(77);
+   added = ls.add(88);
+   added = ls.add(22);
+   added = ls.add(11);
+
+   EXPECT_EQ(added, true);
+   EXPECT_EQ(ls.getCurrentSize(), 5);
+
+   remed = ls.remove(88);
+   EXPECT_EQ(remed, true);
+   EXPECT_EQ(ls.getCurrentSize(), 4);
+   EXPECT_EQ(ls.retrieve(1), 22);
+
+   remed = ls.remove(11);
+   EXPECT_EQ(remed, true);
+   EXPECT_EQ(ls.getCurrentSize(), 3);
+   EXPECT_EQ(ls.contains(11), false);
+}
+
+TEST(SuperclassTest, node_membership) {
+   SortedList<> ls = SortedList();
+   bool added = ls.add(55);
+   added = ls.add(77);
+   added = ls.add(88);
+   added = ls.add(22);
+   added = ls.add(11);
+
+   EXPECT_EQ(added, true);
+   EXPECT_EQ(ls.getCurrentSize(), 5);
+
+   bool match = ls.contains(55);
+
+   EXPECT_EQ(match, true);
+
+   match = ls.contains(88);
+
+   EXPECT_EQ(match, true);
+}
+
+
