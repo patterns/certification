1034f843a529c548c2f4615ebd20477d03fcf0bc
興怡 <>
2025-10-29 22:22:52 -0700

    readme

--- README.md
+++ README.md
@@ -1,11 +1,36 @@
 # CSS501 Program2: Sorted List
+## About
+SortedList is a double linked list of SListNodes.
+ The list head is tagged with the ntype of `HeadNode`, and is linked to the first node AND the last node.
+ Other list nodes that contain data are tagged as ntype of `ItemNode`.
 
-If you use your `driver.cpp` , build with:
+1. Sort order is maintained at the time of insert.
+2. Insert is done by looping through list nodes checking for the item with greater/equal value.
+3. Node items can be fundamental types and specified as template type parameters.
+4. The std::string data type is supported besides fundamental types.
+
+
+## Quickstart
+If you use your `driver3.cpp` , build with:
 
 ```bash
-g++ -std=c++11 *.cpp
+g++ -std=c++11 driver3.cpp
 ./a.out
 ```
+
+
+## Caveats
+1. The sort order for data type std::string is not the expected outcome for numbers.
+   For example, "22" is lower in value than "5" when compared as strings.
+2. Google Test shows SEGFAULT for the std::string template type parameter
+   - Irrespective of C++11 or C++17 setting for CMAKE_CXX_STANDARD
+   - Related to destructor ~SortedList which invokes clear() (and delete)
+   - Build (using g++ -std=c++11) is error-free
+3. The remove method is a simple loop. I realized that with a ordered list it is possible to use binary search but ran out of time
+
+## References
+Move ctor/assignment
+ by [MS](https://learn.microsoft.com/en-us/cpp/cpp/move-constructors-and-move-assignment-operators-cpp?view=msvc-170)
 
 ## Unit tests
 To run unit tests (in the tests subdir):
--- driver3.cpp
+++ driver3.cpp
@@ -1,3 +1,6 @@
+// CSS501:Program2; Hsin-ih Tu; 2025.10.28
+// sorted list with string node item type
+
 #include <iostream>
 #include <string>
 
@@ -6,31 +9,56 @@
 using namespace std;
 
 int main() {
-   SortedList<int> alist;
-   SortedList<string> strlist;
+   SortedList<std::string> lx;
+   SortedList<std::string> ly;
 
-   alist.insert(55);
-   alist.insert(88);
-   alist.insert(22);
-   alist.insert(44);
+   SortedList<std::string> l1;
+   l1.insert("55");
+   l1.insert("77");
+   l1.insert("22");
 
-   cout << alist << endl;
+   SortedList<std::string> l2(l1);    // copy ctor
 
-   SortedList<int> blist(alist);
+   SortedList<std::string> l3;
+   l3.insert("77");
+   l3.insert("99");
+   l3.insert("22");
 
-   cout << blist[1] << endl;
+   // call equals operator
+   bool match = (l1 == l2);
+   cout << "Expect lists (l1, l2) to be equal: " << match << endl;
 
-   strlist.insert("k");
-   strlist.insert("a");
-   strlist.insert("x");
-   strlist.insert("y");
-   strlist.insert("z");
+   bool ident = (l2 == l2);
+   cout << "Expect lists (l2, l2) to be identical: " << ident << endl;
 
-   // equals test
-   SortedList<string> cplist(strlist);
-   if (strlist == cplist) {
-      cout << cplist << endl;
-   }
+   bool miss = (l1 == l3);
+   cout << "Expect lists (l1, l3) to be unequal: " << miss << endl;
+
+   bool empty = (lx == ly);
+   cout << "Expect empty lists (lx, ly) to be equal: " << empty << endl;
+
+
+   // append preparation
+   SortedList<std::string> la1;
+   la1.insert("44");
+   la1.insert("88");
+   la1.insert("22");
+
+   SortedList<std::string> la2;
+   la2.insert("5");
+   la2.insert("9");
+   la2.insert("3");
+
+   // do merge
+   SortedList<std::string> la3 = la1 + la2;
+
+   cout << "Expect size to be 6: " << la3.size() << endl;
+   cout << "Expect item 0 to be 22: " << la3[0] << endl;
+   cout << "Expect item 1 to be 3: " << la3[1] << endl;
+   cout << "Expect item 2 to be 44: " << la3[2] << endl;
+   cout << "Expect item 3 to be 5: " << la3[3] << endl;
+   cout << "Expect item 5 to be 9: " << la3[5] << endl;
+
 
    return 0;
 }
